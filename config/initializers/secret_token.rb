209c49f5e3ffe82bc49aa8e98960210cc6ae79c9
FordeCoM <fordecom@fordecom-SATELLITE-L755.(none)>
2013-06-13 10:25:13 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FormApp::Application.config.secret_token = 'eee17768ec702b43e2c9adacfcb51759eec991a18e304255e0af22fa7411d8be16e153070d28cbad98735954dec7a4ffa95ee255c606bd18d71fea5b1a3809e0'
