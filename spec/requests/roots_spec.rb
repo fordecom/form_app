ef4ba783ee2c5df6616cf852a9e78c73da2c0dde
FordeCoM <fordecom@fordecom-SATELLITE-L755.(none)>
2013-06-14 11:26:57 +0100

    Finish most layouts and routes, just need sections now

--- spec/requests/roots_spec.rb
+++ spec/requests/roots_spec.rb
@@ -2,47 +2,38 @@ require 'spec_helper'
 
 describe "Root" do
 
+  subject { page }
+
   describe "Login page" do
+    before { visit root_path}
 
-    it "should have the h1 'Form App'" do
-      visit '/root/login'
-      page.should have_selector('h1', :text => 'Login')
-    end
-
-    it "should have the base title" do
-      visit '/root/login'
-      page.should have_selector('title', :text => "BCC Form App")
-    end
-
-    it "should not have a custom page title" do
-      visit '/root/login'
-      page.should_not have_selector('title', :text => '| Login')
-    end
+    it { should have_selector('h1', text: 'Login') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Login' }
   end
 
   describe "Logout page" do
 
-    it "should have the h1 'Form App'" do
-      visit '/root/logout'
-      page.should have_selector('h1', :text => 'Logout')
-    end
+    before { visit logout_path}
 
-    it "should have the title 'Logout'" do
-      visit '/root/logout'
-      page.should have_selector('title', :text => "BCC Form App | Logout")
-    end
+    it { should have_selector('h1', text: 'Logout') }
+    it { should have_selector('title', text: full_title('Logout')) }
   end
 
-  describe "Index Page" do 
-   
-    it "should have the h1 'Index'" do
-      visit '/root/index'
-      page.should have_selector('h1', :text => 'Index')
-    end
 
-  it "should have the title 'Index'" do
-      visit '/root/index'
-      page.should have_selector('title', :text => "BCC Form App | Index")
-    end
+  describe "Index Page" do 
+
+    before { visit index_path}
+
+    it { should have_selector('h1', text: 'Index') }
+    it { should have_selector('title', text: full_title('Index')) }
+  end
+
+  describe "Contact page" do
+
+    before { visit contact_path}
+
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
